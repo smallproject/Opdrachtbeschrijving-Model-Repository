3a4b76e48dcef3e72af70d3f20b42432a3b6f487
smallproject <j.a.villas@live.com.ph>
2024-09-12 09:46:58 +0200

    expanded functionality with postmapping - create new television

--- src/main/java/nl/smallproject/www/techiteasy/controllers/TelevisionController.java
+++ src/main/java/nl/smallproject/www/techiteasy/controllers/TelevisionController.java
@@ -2,11 +2,9 @@ package nl.smallproject.www.techiteasy.controllers;
 
 import nl.smallproject.www.techiteasy.models.Television;
 import nl.smallproject.www.techiteasy.services.TelevisionService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,5 +22,11 @@ public class TelevisionController {
     public ResponseEntity<List<Television>> getAllTelevisionsByBrand(@RequestParam(required = false) String brand) {
         List<Television> televisions = televisionService.getAllTelevisionsByBrand(brand);
         return ResponseEntity.ok(televisions);
+    }
+
+    @PostMapping
+    public ResponseEntity<Television> createTelevision(@RequestBody Television television) {
+        Television savedTelevision = televisionService.saveTelevision(television);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedTelevision);
     }
 }
--- src/main/java/nl/smallproject/www/techiteasy/services/TelevisionService.java
+++ src/main/java/nl/smallproject/www/techiteasy/services/TelevisionService.java
@@ -22,4 +22,8 @@ public class TelevisionService {
         return televisionsRepository.findAll();
     }
 
+    public Television saveTelevision(Television television) {
+        return televisionsRepository.save(television);
+    }
+
 }
